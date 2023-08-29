0eb60f195240c82e118021cf1ca066c60dc4042d
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-08-30 00:07:56 +0300

    updated fectch rockets

--- src/redux/slices/RocketsSlice.js
+++ src/redux/slices/RocketsSlice.js
@@ -1,13 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const RocketsbaseUrl = 'https://api.spacexdata.com/v3/rockets';
+
+const initialState = {
+  allRockets: [],
+  isLoading: false,
+};
+
+export const getRockets = createAsyncThunk(
+  'rockets/getRockets',
+  async (name, thunkAPI) => {
+    try {
+      const resp = await axios.get(`${RocketsbaseUrl}`);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Something went wrong');
+    }
+  },
+);
 
 const rocketsSlice = createSlice({
   name: 'rockets',
-  initialState: [],
-  reducers: {
-    setRockets: (state, action) => action.payload,
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getRockets.fulfilled, (state, action) => {
+      const rockets = Object.entries(action.payload).map((items) => ({
+        id: items[0],
+        ...items[1],
+      }));
+      return { ...state, allRockets: rockets };
+    });
+    builder.addCase(getRockets.pending, (state) => ({
+      ...state,
+      isLoading: true,
+    }));
   },
 
 });
 
-export const { setRockets } = rocketsSlice.actions;
 export default rocketsSlice.reducer;
